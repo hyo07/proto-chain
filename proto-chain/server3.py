42aed3418ad74dcce9117bd3c1eaff6a770d5551
hyo07 <yutaka727kato@gmail.com>
2019-06-19 16:37:01 +0900

    fix:server3.py

--- proto-chain/server3.py
+++ proto-chain/server3.py
@@ -4,7 +4,6 @@ import sys
 
 from core.server_core import ServerCore
 
-
 args = sys.argv
 my_p2p_server = None
 
@@ -27,7 +26,7 @@ def main():
         if args[2]:
             my_p2p_server = ServerCore(50091, args[1], int(args[2]))
         elif args[1]:
-         my_p2p_server = ServerCore(50091, args[1])
+            my_p2p_server = ServerCore(50091, args[1])
     except IndexError:
         my_p2p_server = ServerCore(50091)
 
